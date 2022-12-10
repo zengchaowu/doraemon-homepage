fcd487209602acd31879cff66c202e368bd3a0ae
zengchaowu <root@zengchaowu.com>
2022-12-11 00:03:29 +0800

    chore: add TheHeader

---
+++ components/the/header.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="h-16 shadow-sm flex justify-between">
+    <div class="px-10 lg:px-5 flex items-center">
+      <img src="~/assets/images/logo.svg" class="h-8" />
+    </div>
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="absolute inset-0 flex flex-col">
+    <TheHeader />
+    <Nuxt />
+  </div>
+</template>
