f63ee8097621402f66250ebe9673fef5158840ba
zengchaowu <root@zengchaowu.com>
2022-12-11 09:50:21 +0800

    chore: update logo

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="px-12% grid grid-cols-3 gap-8">
-    <div v-for="i in 10" :key="i" class="flex">
+    <div
+      v-for="i in 10"
+      :key="i"
+      class="flex transition-all hover:drop-shadow-2xl"
+    >
       <img
         class="w-full rounded-xl"
         src="https://pixso-public.oss-accelerate.aliyuncs.com/cmt-activity/cover_13?1670292087"
