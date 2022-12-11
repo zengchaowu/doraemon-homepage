cdbe3c02914dd3a55d393a3537f19a4db670e534
zengchaowu <root@zengchaowu.com>
2022-12-11 10:45:21 +0800

    chore: update logo

--- components/the/header.vue
+++ components/the/header.vue
@@ -4,8 +4,9 @@
       class="z-19 fixed bg-white w-full h-16 flex-shrink-0 shadow-sm flex justify-center items-center"
     >
       <div class="max-w-390 flex-grow flex justify-between">
-        <div class="px-5 lg:px-4 flex items-center cursor-pointer">
+        <div class="px-5 lg:px-4 flex gap-2 items-center cursor-pointer">
           <img src="~/assets/images/logo.svg" class="h-8" />
+          <span class="text-2xl font-medium">Dora</span>
         </div>
       </div>
     </div>
